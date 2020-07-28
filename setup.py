d3ebf1c72e72f36522493e1245ac72a1fdca0471
Filipi Nascimento Silva <filipinascimento@gmail.com>
2020-07-27 22:45:40 -0400

    Fix for version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jgf",
-    version="0.2.1",
+    version="0.2.2",
     author="Filipi N. Silva",
     author_email="filipi@iu.edu",
     description="Python library to load JGF files",
